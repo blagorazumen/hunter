e5b6c731a6601c56b3996513977aee3cd34e5c03
blagorazumen <blagorazumn@gmail.com>
2026-03-19 14:36:30 +0500

    Add priceMult to districts and update item price

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
 const DB = {
     districts: {
-        "S": { name: "City (Mirabad)", mult: 10 },
-        "A": { name: "Yakkasaray/M.Ulugbek", mult: 5 },
-        "B": { name: "Chilanzar/Yunusabad", mult: 2 },
-        "C": { name: "Yashnabad/Almazar", mult: 1 },
-        "D": { name: "Sergeli/Bektemir", mult: 0.5 }
+        "S": { name: "City (Mirabad)", mult: 10, priceMult: 10 },
+        "A": { name: "Yakkasaray/M.Ulugbek", mult: 5, priceMult: 5 },
+        "B": { name: "Chilanzar/Yunusabad", mult: 2, priceMult: 2 },
+        "C": { name: "Yashnabad/Almazar", mult: 1, priceMult: 1 },
+        "D": { name: "Sergeli/Bektemir", mult: 0.5, priceMult: 0.5 }
     },
     items: {
         tasks: [
             {
                 id: "cig",
                 name: "Точка штучных сигарет",
-                price: 1000000,
+                basePrice: 50000, // Базовая цена (для района C)
                 income: 16.6,
                 owned: { "S": 0, "A": 0, "B": 0, "C": 0, "D": 0 }
             }
