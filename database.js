fb46ba22d00a1e40bb9eb48dcb5d3f2dba787871
blagorazumen <blagorazumn@gmail.com>
2026-03-19 13:52:01 +0500

    Add initial database structure for tasks

---
+++ database.js
@@ -0,0 +1,17 @@
+const DB = {
+    tasks: [
+        {
+            id: "cigarettes",
+            name: "Сигаретные точки",
+            image: "sigy.png",
+            districts: {
+                "D": { name: "Сергели", price: 1000000, income: 17, count: 0, multiplier: 0.5 },
+                "C": { name: "Яшнабад", price: 5000000, income: 100, count: 0, multiplier: 1 },
+                "B": { name: "Чиланзар", price: 15000000, income: 500, count: 0, multiplier: 2 },
+                "A": { name: "Яккасарай", price: 50000000, income: 2500, count: 0, multiplier: 5 },
+                "S": { name: "Tashkent City", price: 200000000, income: 15000, count: 0, multiplier: 10 }
+            }
+        }
+    ]
+};
+
